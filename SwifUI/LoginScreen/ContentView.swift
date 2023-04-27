4e7717f129361eb4376c2ddaf74ae76b5251fa39
Vítor José <83792704+vitorjoseph@users.noreply.github.com>
2023-04-27 14:34:36 -0300

    Login

---
+++ SwifUI/LoginScreen/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  LoginScreen
+//
+//  Created by Federico on 13/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+                LoginView()
+            
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
+
+
+
+
+
+
+
