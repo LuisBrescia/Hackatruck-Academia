9a43c47c0fa04d79b81546b48f9c2776ec331a03
Pedro Márcio <66282747+pedromos16@users.noreply.github.com>
2023-04-28 16:22:25 -0300

    Update ContentView.swift
    Criação do botão para criar fichas

--- SwifUI/projeto-final/projeto-final/ContentView.swift
+++ SwifUI/projeto-final/projeto-final/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var exibeAdicionarFicha = false
     var body: some View {
         TabView {
-            PaginaFicha()
-                .tabItem {
-                    Label("Ficha Treino", systemImage: "list.bullet.clipboard.fill")
-                }
+            NavigationView {
+                PaginaFicha()
+                    .navigationBarTitle("Ficha Treino")
+                    .navigationBarItems(trailing: Button(action: {
+                        self.exibeAdicionarFicha = true
+                    }) {
+                        Image(systemName: "plus")
+                    })
+            }
+            .tabItem {
+                Label("Ficha Treino", systemImage: "list.bullet.clipboard.fill")
+            }
             
             PaginaExercicios()
                 .tabItem {
@@ -25,11 +34,40 @@ struct ContentView: View {
                     Label("Ferramentas", systemImage: "hammer.circle.fill")
                 }
         }
+        .sheet(isPresented: $exibeAdicionarFicha) {
+            AdicionarFicha()
+        }
     }
+
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+
+struct AdicionarFicha: View {
+    @State private var nomeFicha = ""
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Informações da Ficha")) {
+                    TextField("Nome da Ficha", text: $nomeFicha)
+                    
+                }
+                
+                Section {
+                    Button(action: {
+                        // Aqui você pode implementar o código para salvar as informações da ficha
+                    }) {
+                        Text("Salvar")
+                    }
+                }
+            }
+            .navigationBarTitle("Adicionar Ficha")
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
