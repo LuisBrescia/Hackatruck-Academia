e30acd7fa867f3bfd710246060999f83d704e1c6
Luís Brescia <luisfelipebrescia@gmail.com>
2023-05-08 16:28:19 -0300

    Create Objeto.swift

---
+++ FitLife/Objeto.swift
@@ -0,0 +1,16 @@
+// Nesse arquivo será declarado as classes utilizadas
+
+import Foundation
+
+struct Exercicio: Hashable {
+    var nome: String
+    var series: Int
+    var carga: Double
+    var anotacao: String
+    var foto: String
+}
+
+struct Ficha: Hashable {
+    var nome: String
+    var exercicios: [Exercicio]
+}
