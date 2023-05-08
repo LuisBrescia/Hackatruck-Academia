385bb13315476e409d22a44c6280be3a35d58d68
Luís Brescia <luisfelipebrescia@gmail.com>
2023-05-08 16:30:21 -0300

    Create Exercicios.swift

---
+++ FitLife/TabView/Exercicios.swift
@@ -0,0 +1,20 @@
+//
+//  Exercicios.swift
+//  FitLife
+//
+//  Created by Student15 on 08/05/23.
+//
+
+import SwiftUI
+
+struct Exercicios: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Exercicios_Previews: PreviewProvider {
+    static var previews: some View {
+        Exercicios()
+    }
+}
