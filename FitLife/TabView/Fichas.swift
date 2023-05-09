45bd869a6f6942d3b347a9f9f37cb40a7aa6ff40
Luís Brescia <luisfelipebrescia@gmail.com>
2023-05-09 16:03:32 -0300

    Update Fichas.swift

--- FitLife/TabView/Fichas.swift
+++ FitLife/TabView/Fichas.swift
@@ -1,5 +1,6 @@
-// Neste arquivo contém todo o conteúdo para rodar
-// a primeira página do TabView
+// Primeira página do aplicativo.
+// Conterá as fichas de cada usuário.
+// Nela será possível criar, acessar, e alterar fichas já personalizadas.
 
 import SwiftUI
 
@@ -10,25 +11,25 @@ struct Fichas: View {
         Ficha(
             nome: "Dia A - Peito, Triceps e Ombros",
             exercicios: [
-                Exercicio(nome: "Supino Inclinado com Halteres", series: 3, carga: 10, anotacao: "Aqui ficam as Anotações", foto: "https://grandeatleta.com.br/wp-content/uploads/2022/07/supino-inclinado-com-halteres.jpg"),
-                Exercicio(nome: "Tríceps Máquina", series: 3, carga: 10, anotacao: "Aqui ficam as Anotações", foto: "https://www.mundoboaforma.com.br/wp-content/uploads/2021/07/triceps-sentado-no-aparelho.gif"),
-                Exercicio(nome: "Desenvolvimento Máquina", series: 3, carga: 10, anotacao: "Aqui ficam as Anotações", foto: "https://i.pinimg.com/originals/3c/72/55/3c7255660b71703615d7c7e037eab853.png")
+                Exercicio(nome: "Supino Inclinado com Halteres", series: 3, carga: 10, anotacao: ant, foto: fotos[0]),
+                Exercicio(nome: "Tríceps Máquina", series: 3, carga: 10, anotacao: ant, foto: fotos[1]),
+                Exercicio(nome: "Desenvolvimento Máquina", series: 3, carga: 10, anotacao: ant, foto: fotos[2])
             ]
         ),
         Ficha(
             nome: "Dia B - Pernas",
             exercicios: [
-                Exercicio(nome: "Leg Press 45", series: 3, carga: 10, anotacao: "Aqui ficam as Anotações", foto: "https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/pernas-leg-press-45-tradicional-90-graus.gif"),
-                Exercicio(nome: "Agachamnento Máquina", series: 3, carga: 10, anotacao: "Aqui ficam as Anotações", foto: "https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/agachamento-smith-machine-com-foco-nos-gluteos-e-posteriores-da-coxa.gif"),
-                Exercicio(nome: "Gêmeos Sentado", series: 3, carga: 10, anotacao: "Aqui ficam as Anotações", foto: "https://ginasiovirtual.com/wp-content/uploads/2021/06/Elevacao-de-gemeos-em-maquina-sentado.jpg")
+                Exercicio(nome: "Leg Press 45", series: 3, carga: 10, anotacao: ant, foto: fotos[3]),
+                Exercicio(nome: "Agachamnento Máquina", series: 3, carga: 10, anotacao: ant, foto: fotos[4]),
+                Exercicio(nome: "Gêmeos Sentado", series: 3, carga: 10, anotacao: ant, foto: fotos[5])
             ]
         ),
         Ficha(
             nome: "Dia C - Costas e Bíceps",
             exercicios: [
-                Exercicio(nome: "Remada Máquina", series: 3, carga: 10, anotacao: "Aqui ficam as Anotações", foto: "https://treinomestre.com.br/wp-content/uploads/2021/11/remada-articulada-.jpg"),
-                Exercicio(nome: "Rosca Scott", series: 3, carga: 10, anotacao: "Aqui ficam as Anotações", foto: "https://treinomestre.com.br/wp-content/uploads/2016/11/rosca-scott.jpg"),
-                Exercicio(nome: "Crucifixo Inverso Máquina", series: 3, carga: 10, anotacao: "Aqui ficam as Anotações", foto: "https://treinomestre.com.br/wp-content/uploads/2018/09/crucifixo-invertido-maquina.jpg.webp")
+                Exercicio(nome: "Remada Máquina", series: 3, carga: 10, anotacao: ant, foto: fotos[6]),
+                Exercicio(nome: "Rosca Scott", series: 3, carga: 10, anotacao: ant, foto: fotos[7]),
+                Exercicio(nome: "Crucifixo Inverso Máquina", series: 3, carga: 10, anotacao: ant, foto: fotos[8])
             ]
         )
     ]
@@ -36,20 +37,13 @@ struct Fichas: View {
     var body: some View {
         NavigationView {
             // Vai printar na tela todas as fichas salvas
-            List(ListaFicha, id: \.self) { ficha in
-                // Quando elemento for ativado
-                NavigationLink(ficha.nome) {
+            List(ListaFicha, id: \.self) {
+                ficha in NavigationLink(ficha.nome) {
                     // Chamará outra página passando a ficha em questão como parametro
                     ExibeFicha(exercicios: ficha.exercicios)
                 }
-            }.navigationTitle("Ficha")}
-    }
-}
-
-// Carregar página com as fichas salvas
-struct Fichas_Previews: PreviewProvider {
-    static var previews: some View {
-        Fichas()
+            }.navigationTitle("Ficha")
+        }
     }
 }
 
@@ -84,19 +78,17 @@ struct ExercicioFicha: View {
             } placeholder: {
                 ProgressView()
             }
-            .frame(width: 250, height: 250)
+.frame(width: 250, height: 250)
             
             VStack(alignment: .leading) {
                 Text(exercicio.nome)
                     .font(.title)
                     .fontWeight(.bold)
-                
                 HStack {
                     Text("\(exercicio.series)x")
                     Text("-")
                     Text(String(format: "%.1fkg", exercicio.carga))
                 }
-                
                 Button("Anotação") {
                     showingSheet.toggle()
                 }
@@ -104,7 +96,6 @@ struct ExercicioFicha: View {
                     Text(exercicio.anotacao)
                         .presentationDetents([.medium, .large])
                 }
-                
                 Toggle("Feito: ", isOn: $isOn)
             }
             
@@ -112,16 +103,32 @@ struct ExercicioFicha: View {
     }
 }
 
-struct ExercicioFicha_Previews: PreviewProvider {
+// DESENVOLVIMENTO
+struct Fichas_Previews: PreviewProvider {
     static var previews: some View {
-        let exercicio = Exercicio(
-            nome: "Rosca reta direta",
-            series: 3,
-            carga: 15,
-            anotacao: "Anotação",
-            foto: "https://grandeatleta.com.br/wp-content/uploads/2018/07/rosca-direta-barra-reta-ou-w.jpg"
-        )
-        ExercicioFicha(exercicio: exercicio)
+        Fichas()
     }
 }
 
+var fotos = [
+    // Supino Inclinado
+    "https://grandeatleta.com.br/wp-content/uploads/2022/07/supino-inclinado-com-halteres.jpg",
+    // Tríceps Máquina
+    "https://www.mundoboaforma.com.br/wp-content/uploads/2021/07/triceps-sentado-no-aparelho.gif",
+    // Desenvolvimento Máquina
+    "https://i.pinimg.com/originals/3c/72/55/3c7255660b71703615d7c7e037eab853.png",
+    // Leg Press 45
+    "https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/pernas-leg-press-45-tradicional-90-graus.gif",
+    // Agachamento Máquina
+    "https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/agachamento-smith-machine-com-foco-nos-gluteos-e-posteriores-da-coxa.gif",
+    // Gemeos Sentando
+    "https://ginasiovirtual.com/wp-content/uploads/2021/06/Elevacao-de-gemeos-em-maquina-sentado.jpg",
+    // Remada Máquina
+    "https://treinomestre.com.br/wp-content/uploads/2021/11/remada-articulada-.jpg",
+    // Rosca Scott
+    "https://treinomestre.com.br/wp-content/uploads/2016/11/rosca-scott.jpg",
+    // Crucifixo Inverso Máquina
+    "https://treinomestre.com.br/wp-content/uploads/2018/09/crucifixo-invertido-maquina.jpg.webp"
+]
+
+var ant = "Aqui ficam as anotações"
